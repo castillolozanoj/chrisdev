6b3343565eea26d24a0654ea3db3d4de2ef6175d
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2021-05-10 20:23:12 -0500

    update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const helmet = require("helmet");
 const http = require("http").createServer(app);
-//const express_enforces_ssl = require("express-enforces-ssl");
+const express_enforces_ssl = require("express-enforces-ssl");
 const hostValidation = require("host-validation");
 
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +27,7 @@ app.set("view engine", "handlebars");
 
 //middleware
 app.enable("trust proxy");
-//app.use(express_enforces_ssl());
+app.use(express_enforces_ssl());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(favicon(path.join(__dirname, "public/img", "favicon.ico")));
