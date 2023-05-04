55c9ecaefd5c969eaed6c1acf055cdf775064ba6
Jesús Christian Castillo Lozano <christian.castillo.lozano@gmail.com>
2023-05-04 04:11:19 +0000

    REF: se corrige la ruta principal

--- src/routes/data/dataMetaTags.js
+++ src/routes/data/dataMetaTags.js
@@ -3,7 +3,7 @@ module.exports = {
   metaKeyword: 'Jesus Christian Castillo Lozano developer javascript desarrollo coacalco México mexico estadodeméxico estado-de-mexico',
   metaDescription: ' Soy Jesús Christian Castillo Lozano, desarrollador JavaScript apasionado por mejorar el mundo y aumentar la energía. En mi sitio web, encontrarás información sobre mis habilidades de desarrollo, proyectos, y mi objetivo de utilizar la tecnología para crear un mundo más sostenible. ¡Conoce más sobre mí y mi trabajo en mi sitio web personal!',
   ogTitle: 'Jesús Christian Castillo Lozano - Desarrollador de JavaScript | Energía sostenible y tecnología',
-  ogUrl: 'https://castillolozano.ddns.net/desarrollador-javascript-energia-sostenible',
+  ogUrl: 'https://castillolozano.ddns.net',
   ogDescription: 'Descubre mis habilidades como desarrollador de JavaScript y mi objetivo de utilizar la tecnología para crear un mundo más sostenible. Visita mi sitio web personal para conocer más sobre mi trabajo y cómo podemos juntos aumentar la energía en nuestro planeta.',
   ogImage: 'https://castillolozano.ddns.net/images/logo_chris1.webp',
   ogType: 'profile site'
